6fe4e9761e8de20f4dfbb78e0ebbe1519e4d54a6
Investmentbanking <ayesha.dorani@kcl.ac.uk>
2023-09-23 21:44:33 +0100

    Frequency counter update

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
+
+    for item in items:
+        if(str(item) in frequencies):
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1
     # Your code goes here
     return frequencies
